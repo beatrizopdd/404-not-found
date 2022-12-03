69bb88b998450cb668e33ad9d6a41c6339181b74
arthurbougleux <arthur_lbs@id.uff.br>
2022-12-03 16:51:53 -0300

    Lógica pronta

--- menu_fases.py
+++ menu_fases.py
@@ -12,32 +12,38 @@ def menu_fases(tela, teclado, mouse):
     #Sons
     som_startup = Sound("Assets/Audios/Efeitos/startup.ogg")
     som_invalido = Sound("Assets/Audios/Efeitos/botão_esquisito.ogg")
+    som_erro = Sound("Assets/Audios/Efeitos/erro.ogg")
     som_desligando = Sound("Assets/Audios/Efeitos/desligando.ogg")
 
     #Imagems
-    fundo = GameImage("Assets/Fundos/menu fundo.png")
     menu = GameImage("Assets/Menu/menu_puro.png")
 
-    centralizar_na_tela(menu, tela)
-
     #Botões
-    play = Sprite("Assets/Menu/play.png")
-    play_press = Sprite("Assets/Menu/play_press.png")
 
-    about = Sprite("Assets/Menu/about.png")
-    about_press = Sprite("Assets/Menu/about_press.png")
+    fase1 = Sprite("Assets/Menu/x.png")
+    fase1_press = Sprite("Assets/Menu/x_press.png")
+
+    fase2 = Sprite("Assets/Menu/x.png")
+    fase2_press = Sprite("Assets/Menu/x_press.png")
+
+    fase3 = Sprite("Assets/Menu/x.png")
+    fase3_press = Sprite("Assets/Menu/x_press.png")
 
     x = Sprite("Assets/Menu/x.png")
     x_press = Sprite("Assets/Menu/x_press.png")
 
-    interrogação = Sprite("Assets/Menu/?.png")
-    interrogação_press = Sprite("Assets/Menu/?_press.png")
+    interrogação = Sprite("Assets/Menu/interrogacao.png")
+    interrogação_press = Sprite("Assets/Menu/interrogacao_press.png")
 
-    botões_principais = [play, about, interrogação, x]
-    botões_secundários = [play_press, about_press, interrogação_press, x_press]
+    botões_principais = [fase1, fase2, fase3, interrogação, x]
+    botões_secundários = [fase1_press, fase2_press, fase3_press, interrogação_press, x_press]
 
     #Colocando os botões no lugar
-    alinha_botões(botões_principais, menu, menu.height/1.5)
+    for b in botões_principais:
+
+        b.y = menu.y + 3 * menu.width/5
+
+    alinha_botões_horizontal(botões_principais, menu, menu.width/6, 4)
 
     #O hardcode é feio, mas não valia fazer uma função só pra alinhas esses dois
     #? e X estavam 15 pixels abaixo do topo do menu na imagem original
@@ -53,6 +59,7 @@ def menu_fases(tela, teclado, mouse):
 
     #Declarações genéricas
     clickou_em = -1
+    soltou_mouse = True
 
 
     while True:
@@ -62,44 +69,61 @@ def menu_fases(tela, teclado, mouse):
             fechar_jogo(tela)
 
         #Procurando por clicks nos botões e mudando eles visualmente
-        for i in range(len(botões_principais)):
+        if mouse.is_over_object(menu):
 
-            if mouse.is_over_object(botões_principais[i]) and mouse.is_button_pressed(1):
+            for i in range(len(botões_principais)):
 
-                botões_principais[i].hide()
-                botões_secundários[i].unhide()
+                if mouse.is_over_object(botões_principais[i]) and mouse.is_button_pressed(1):
 
-                clickou_em = i
-            
-            elif clickou_em != i:
+                    botões_principais[i].hide()
+                    botões_secundários[i].unhide()
 
-                botões_principais[i].unhide()
-                botões_secundários[i].hide()
+                    clickou_em = i
+                
+                elif clickou_em != i:
+
+                    botões_principais[i].unhide()
+                    botões_secundários[i].hide()
+        
+
+        #Lógica do som inválido quando clicka fora da janela
+        if mouse.is_button_pressed(1) and not mouse.is_over_object(menu) and soltou_mouse:
+
+            som_erro.play()
+            soltou_mouse = False
+        
+        elif not mouse.is_button_pressed(1):
+
+            soltou_mouse = True
+
 
         #Executando o comando relacionado ao botão quando o jogador solta o mouse
         if clickou_em != -1 and not mouse.is_button_pressed(1):
 
-            if clickou_em == 0: #Se clickou em jogar
+            if clickou_em == 0: #Se clickou na fase 1
 
                 som_invalido.play()
 
-            if clickou_em == 1: #Se clickou em about
+            if clickou_em == 1: #Se clickou na fase 2
 
                 som_invalido.play()
 
-            if clickou_em == 2: #Se clickou na ?
+            if clickou_em == 2: #Se clickou na fase 3
 
                 som_invalido.play()
 
-            if clickou_em == 3: #Se clickou no X
+            if clickou_em == 3: #Se clickou em ?
 
-                fechar_jogo(tela)
+                som_invalido.play()
+
+            if clickou_em == 4: #Se clickou no X
+
+                return
 
             clickou_em = -1
 
 
         #Desenhando coisas
-        fundo.draw()
         menu.draw()
 
         for i in range(len(botões_secundários)):
